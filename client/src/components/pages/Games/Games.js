a8a2069708f76142f277db54c7302d9f01c77104
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-22 22:19:19 -0600

    added Games and Music js and json files in pages folder, used Art js and json as examples

--- client/src/components/pages/Games/Games.js
+++ client/src/components/pages/Games/Games.js
@@ -1,12 +1,42 @@
 import React from "react";
+import Card from "../../Card"
+import Wrapper from "../../Wrapper";
+import games from "./games.json"
 
-const Games = () => (
-  <div>
-    <h1>Board and Tabletop Games</h1>
-    <p>
-different board games, chess, checkers, etc
-    </p>
-  </div>
-);
+// const Games = () => (
+//   <div>
+//     <h1>Board and Tabletop Games</h1>
+//     <p>
+// different board games, chess, checkers, etc
+//     </p>
+//   </div>
+// );
+
+
+
+class Art extends Component {
+
+  state = {
+    games
+  };
+
+  render() {
+return (
+    <div>
+      <h1>Games</h1>
+      {games.map(hobby=> (
+        <Card
+          id={hobby.id}
+          key={hobby.id}
+          name={hobby.name}
+          image={hobby.image}
+          cost={hobby.cost}
+          description={hobby.description}
+        />
+      ))}
+    </div>
+
+) }
+      }
 
 export default Games;
--- client/src/components/pages/Music/Music.js
+++ client/src/components/pages/Music/Music.js
@@ -1,12 +1,40 @@
 import React from "react";
+import Card from "../../Card"
+import Wrapper from "../../Wrapper";
+import music from "./music.json"
 
-const Music = () => (
-  <div>
-    <h1>Music</h1>
-    <p>
-Guitar, Write a song, concerts, music history and theory
-    </p>
-  </div>
-);
+// const Music = () => (
+//   <div>
+//     <h1>Music</h1>
+//     <p>
+// Guitar, Write a song, concerts, music history and theory
+//     </p>
+//   </div>
+// );
+
+class Art extends Component {
+
+  state = {
+    music
+  };
+
+  render() {
+return (
+    <div>
+      <h1>Music</h1>
+      {music.map(hobby=> (
+        <Card
+          id={hobby.id}
+          key={hobby.id}
+          name={hobby.name}
+          image={hobby.image}
+          cost={hobby.cost}
+          description={hobby.description}
+        />
+      ))}
+    </div>
+
+) }
+      }
 
 export default Music;

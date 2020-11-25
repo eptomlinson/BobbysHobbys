cadf3ed431f00a6eb211b0a1b9de4f8589dc8598
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-25 13:40:24 -0600

    finished dummy data in json files, edited older files as well

--- client/src/components/pages/Games/Games.js
+++ client/src/components/pages/Games/Games.js
@@ -3,45 +3,45 @@ import Card from "../../Card"
 import Wrapper from "../../Wrapper/index";
 import games from "./games.json"
 
-// const Games = () => {
-//   const [gamesState, setGamesState] = React.useState(games)
-//   return (
-//   <div>
-//     <h1>Board and Tabletop Games</h1>
-//     <p>
-// different board games, chess, checkers, etc
-//     </p>
-//   </div>
-//   )
-//   };
-
-
-
-class Games extends Component {
-
-  state = {
-    games
+const Games = () => {
+  const [gamesState, setGamesState] = React.useState(games)
+  return (
+  <div>
+    <h1>Board and Tabletop Games</h1>
+    <p>
+different board games, chess, checkers, etc
+    </p>
+  </div>
+  )
   };
 
-  render() {
-return (
-  <div>
-     <h1>Games</h1>
-    <Wrapper>
-      {games.map(hobby=> (
-        <Card
-          id={hobby.id}
-          key={hobby.id}
-          name={hobby.name}
-          image={hobby.image}
-          cost={hobby.cost}
-          description={hobby.description}
-        />
-        ))}
+
+
+// class Games extends Component {
+
+//   state = {
+//     games
+//   };
+
+//   render() {
+// return (
+//   <div>
+//      <h1>Games</h1>
+//     <Wrapper>
+//       {games.map(hobby=> (
+//         <Card
+//           id={hobby.id}
+//           key={hobby.id}
+//           name={hobby.name}
+//           image={hobby.image}
+//           cost={hobby.cost}
+//           description={hobby.description}
+//         />
+//         ))}
    
-        </Wrapper>
-        </div>
-    ) }
-          }
+//         </Wrapper>
+//         </div>
+//     ) }
+//           }
 
 export default Games;
